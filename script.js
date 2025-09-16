6d575ab49754a248b77cfdc955c7e7af77a52449
techie-emmax <emeraldkreative@gmail.com>
2025-09-16 07:13:51 +0100

    fix:added some changes

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+    const menuButton = document.getElementById('menu');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    menuButton.addEventListener('click', ()=>{
+        mobileMenu.classList.toggle('hidden');
+    });
+
+
